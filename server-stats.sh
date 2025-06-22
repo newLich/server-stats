5d1f3cf1881a00c6a1bdc18a8bea91033d86397d
newLich <lichston1@gmail.com>
2025-06-22 20:00:09 -0300

    First commit, monitoring CPU, Memory free and used

---
+++ server-stats.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PORCENTAGEM="CPU%: "
+MEMORIA=$(free '-h' | grep 'Mem')
+
+while true;
+	do echo -n -e '\e[31;1m'$PORCENTAGEM' \e[m' && top '-bn1' | grep '%Cpu(s):' | awk '{print $2}'
+	echo -n -e '\e[34;1mMemoria utilizada: \e[m'  && echo $MEMORIA | awk '{print $3}' && echo -n -e '\e[36;1mMemoria Livre: \e[m' && echo $MEMORIA | awk '{print $4}'
+	sleep 2
+done
