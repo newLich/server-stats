66d1f21f579046d67f20449fb682b11075422682
newLich <lichston1@gmail.com>
2025-06-23 16:47:30 -0300

    Adding Memory monitoring by process and the memory by percent

--- server-stats.sh
+++ server-stats.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
 PORCENTAGEM="CPU%: "
-MEMORIA_USADA=$(free '-h' | grep 'Mem' | awk '{print $3}')
-MEMORIA_LIVRE=$(free '-h' | grep 'Mem' | awk '{print $4}')
-MEMORIA_TOTAL=$(free '-h' | grep 'Mem' | awk '{print $2}' | awk -F 'G' '{print $1}')
+
+MEMORIA_USADA_H=$(free -h | grep 'Mem' | awk '{print $3}')
+MEMORIA_LIVRE_H=$(free -h | grep 'Mem' | awk '{print $4}')
+
+typeset -i MEMORIA_USADA=$(free | grep 'Mem' | awk '{print $3}')
+typeset -i MEMORIA_LIVRE=$(free | grep 'Mem' | awk '{print $4}')
+typeset -i MEMORIA_TOTAL=$(free | grep 'Mem' | awk '{print $2}')
 
 while true;
-	do echo -n -e '\e[31;1m'$PORCENTAGEM' \e[m' && top '-bn1' | grep '%Cpu(s):' | awk '{print $2}'
-	echo -n -e '\e[34;1mMemoria utilizada: \e[m'  && echo $MEMORIA_USADA
-	echo -n -e '\e[36;1mMemoria Livre: \e[m' && echo $MEMORIA_LIVRE
-	echo -e '\e[36;1mTOP PROCESSOS: \e[m' && top -bn1 | grep -E '[% [0-9]{1,2}[:][0-9]{1,2}[.][0-9]{1,2}*' | head -n 5
+	do
+
+	MEMORIA_USADA_PORCENTAGEM=$(echo "scale=2; $MEMORIA_USADA / $MEMORIA_TOTAL" | bc)
+	MEMORIA_LIVRE_PORCENTAGEM=$(echo "scale=2; $MEMORIA_LIVRE / $MEMORIA_TOTAL" | bc)
+
+	echo -n -e '\n''\e[31;1m'$PORCENTAGEM' \e[m' && top '-bn1' | grep '%Cpu(s):' | awk '{print $2}'
+	echo -n -e '\e[34;1mMemoria utilizada: \e[m' &&  echo -n $MEMORIA_USADA_H " " && echo $MEMORIA_USADA_PORCENTAGEM'%' | sed -E 's/.//'
+	echo -n -e '\e[36;1mMemoria Livre: \e[m' && echo -n $MEMORIA_LIVRE_H " " && echo $MEMORIA_LIVRE_PORCENTAGEM'%' | sed -E 's/.//'
+	echo -e '\n''\e[36;1mTop Processos CPU: \e[m' && top -bn1 -o %CPU | grep -E '[% [0-9]{1,2}[:][0-9]{1,2}[.][0-9]{1,2}*' | head -n 5
+	echo -e '\n''\e[36;1mTop Processos MEMORIA: \e[m' && top -bn1 -o %MEM | grep -E '[% [0-9]{1,2}[:][0-9]{1,2}[.][0-9]{1,2}*' | head -n 5
 	sleep 2
+
 done
