28d0b8a790b1926ba6ce2084d7548d992ec3e046
newLich <lichston1@gmail.com>
2025-06-22 23:17:51 -0300

    Add the top 5 most heavy CPU process

--- server-stats.sh
+++ server-stats.sh
@@ -6,5 +6,6 @@ MEMORIA=$(free '-h' | grep 'Mem')
 while true;
 	do echo -n -e '\e[31;1m'$PORCENTAGEM' \e[m' && top '-bn1' | grep '%Cpu(s):' | awk '{print $2}'
 	echo -n -e '\e[34;1mMemoria utilizada: \e[m'  && echo $MEMORIA | awk '{print $3}' && echo -n -e '\e[36;1mMemoria Livre: \e[m' && echo $MEMORIA | awk '{print $4}'
+	echo -e '\e[36;1mTOP PROCESSOS: \e[m' && top -bn1 | grep -E '[% [0-9]{1,2}[:][0-9]{1,2}[.][0-9]{1,2}*' | head -n 5
 	sleep 2
 done
